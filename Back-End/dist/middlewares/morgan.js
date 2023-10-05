9d8057cdf80385d1ad26b6213cc7e3a367739b8c
Adri Antori <adriantori11@gmail.com>
2023-10-05 11:28:19 +0700

    etc: redeploy

--- Back-End/dist/middlewares/morgan.js
+++ Back-End/dist/middlewares/morgan.js
@@ -4,10 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const morgan_1 = __importDefault(require("morgan"));
-const fs_1 = __importDefault(require("fs"));
 exports.default = (app) => {
-    app.use((0, morgan_1.default)('combined', { stream: fs_1.default.createWriteStream('./access.log', { flags: 'a' }) } //create file
-    //{db.insertOne} //push database
-    ));
+    // app.use(logger('combined', 
+    //     {stream: fs.createWriteStream('./access.log', {flags: 'a'})} //create file
+    //     {db.insertOne} //push database
+    // ))
     app.use((0, morgan_1.default)('dev'));
 };
--- Back-End/src/middlewares/morgan.ts
+++ Back-End/src/middlewares/morgan.ts
@@ -2,9 +2,9 @@ import { Express } from "express";
 import logger from 'morgan';
 
 export default (app: Express) => {
-    app.use(logger('combined', 
-        //{stream: fs.createWriteStream('./access.log', {flags: 'a'})} //create file
-        //{db.insertOne} //push database
-    ))
+    // app.use(logger('combined', 
+    //     {stream: fs.createWriteStream('./access.log', {flags: 'a'})} //create file
+    //     {db.insertOne} //push database
+    // ))
     app.use(logger('dev'))
 }
