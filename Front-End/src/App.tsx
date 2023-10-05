30a736d4fc1109c8b2b2db2b80b1a94d075088a7
Adri Antori <adriantori11@gmail.com>
2023-10-05 17:13:48 +0700

    feat: adding clickjacking prevention

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,10 +1,16 @@
 import './App.css'
 import { Get, Post, Put, Delete } from './functions'
+import Helmet from "react-helmet"
 
 function App() {
 
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Adri Antori W-15</title>
+        <meta name="description" content="RevoU Week 15" />
+      </Helmet>
       <button onClick={Get}>GET</button>
       <button onClick={Post}>POST</button>
       <button onClick={Put}>PUT</button>
