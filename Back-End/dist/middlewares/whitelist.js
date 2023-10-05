68536950e9832e73299c49e89cebe838aa0d4d05
Adri Antori <adriantori11@gmail.com>
2023-10-05 12:00:14 +0700

    etc: testing global CORS

--- Back-End/dist/middlewares/whitelist.js
+++ Back-End/dist/middlewares/whitelist.js
@@ -6,7 +6,7 @@ const whitelist = {
         methods: ['GET', 'POST']
     },
     clientOptionsGlobal: {
-        origin: 'https://adriantori-w15-b.vercel.app',
+        origin: '*',
         methods: ['GET', 'POST', 'PUT', 'DELETE']
     }
 };
